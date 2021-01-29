d176d2e019e29a164404799f97a88dabb8ecc885
Iresh M Dissanayaka <socialexpz1@gmail.com>
2021-01-29 17:40:04 +0530

    update encryption config file extension from yml to yaml

--- encryption-config/gen-encryption-config.sh
+++ encryption-config/gen-encryption-config.sh
@@ -1,3 +1,3 @@
 ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
-sed "s/ENCRYPTION_KEY/${ENCRYPTION_KEY}/" encryption-config.template.yml  > encryption-config.yml
+sed "s/ENCRYPTION_KEY/${ENCRYPTION_KEY}/" encryption-config.template.yml  > encryption-config.yaml
